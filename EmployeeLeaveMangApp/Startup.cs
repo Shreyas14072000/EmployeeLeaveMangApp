c731f81dc9add2a0faba69a3df22b4e7791d7220
Shreyas14072000 <100415922+Shreyas14072000@users.noreply.github.com>
2022-04-09 11:53:03 +0530

    hi

--- EmployeeLeaveMangApp/Startup.cs
+++ EmployeeLeaveMangApp/Startup.cs
@@ -1,6 +1,8 @@
+using EmployeeLeaveMangApp.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace EmployeeLeaveMangApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<InterfaceEmployeeService, EmployeeService>();
+
+            services.AddDbContext<ApplicationEmployeeContext>(x => x.UseSqlServer(Configuration.GetConnectionString("ConStr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
